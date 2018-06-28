a03085b815d2a80c2cb00a079a99bc13499173d8
Aaron Robson <ARobsonSE@hotmail.co.uk>
2018-06-28 10:57:54 +0100

    Fixed unusual location of the SortingRoutines project being outside of the repo.

---
+++ SortingRoutines/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SortingRoutines
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using SortingRoutines.Library;
+
+    class Program
+    {
+        private static readonly List<int> Given = new List<int> { 3, 2, 4, 1 };
+
+        private static string ListRepresentation<T>(List<T> values)
+        {
+            return string.Join(", ", values.Select(x => x.ToString()));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Given:\n{ListRepresentation(Given)}\n");
+
+            var bubbleSort = new BubbleSort();
+            var Actual = bubbleSort.Sort(Given);
+            Console.WriteLine($"Bubble Sort Result:\n{ListRepresentation(Actual)}\n");
+
+            Console.WriteLine("Press any key to exit:");
+            Console.Read();
+        }
+    }
+}
